63788e01fb43bee5db40c718a6e824d49207ab90
Denny <311210011@student.machung.ac.id>
2021-01-08 12:15:25 +0700

    final

--- demoarr.py
+++ demoarr.py
@@ -1,45 +1,46 @@
 import sys
-from prettytable import PrettyTable 
 
-t = PrettyTable()
-
-t.field_names = ["Tanggal", "Nama Transaksi", "Jumlah Transaksi"]
-t.add_row([0, "Indomaret", 30000])
-t.add_row([1, "Alfamart", 4999])
+listTemp = []
 jumlah = 0
 transaksi = []
 nama = input("Halo user silahkan input nama anda :")
 bulan = input("inputkan bulan pembukuan :")
 
-def insert():
-    x = len(listTemp)
-    listTemp.insert(x+1, transaksi)
-    tanggal = int(input())
-    namaTransaksi = str(input())
-    jumlahTransaksi = int(input())
-
 while True:
+    x = len(listTemp)
     print('tekan (1) untuk input data, (2) untuk print data input, (q) keluar program')
     userinput = input()
     if userinput == 'q':
         sys.exit()
     if userinput == '2':
-        print("==================================")
-        print("  Laporan "+nama+" dibulan "+bulan+"  ")
-        print("==================================")
-        print(t)
-        print("Total ===========")
-        break
+        print("===========================================|")
+        print("       Laporan "+nama+" dibulan "+bulan+"          |")
+        print("===========================================|")
+        print("Tanggal | Nama Transaksi  |     Jumlah     |")
+        print("-------------------------------------------|")
+        for r in listTemp:
+            for c in r:
+                print(c, end="       |      ")
+            print()
+        for tanggal, namaTransaksi, jumlahTransaksi in listTemp:
+            jumlah += jumlahTransaksi
+        print("-------------------------------------------|")
+        print("Total:                          " + str(jumlah)+"      |")
+        print("-------------------------------------------|")
     if userinput == '1':
-        x = len(listTemp)
-        print(x)
-        listTemp.insert(x+1, transaksi)
-        tanggal = int(input("tanggal transaksi"))
-        namaTransaksi = input("Jenis transaksi : ")
-        jumlahTransaksi = int(input("Jumlah transaksi : "))
-
+        tanggal = int(input("Input tanggal transaksi : "))
+        namaTransaksi = input("Input Jenis transaksi : ")
+        jumlahTransaksi = int(input("Input Jumlah transaksi : "))
         transaksi.append(tanggal)
         transaksi.append(namaTransaksi)
         transaksi.append(jumlahTransaksi)
-        listTemp.append(transaksi)
+        if x == 0:
+             arr = 0
+             listTemp.insert(arr,transaksi)
+        else:
+             arr = x-1
+             listTemp.insert(arr+1, transaksi)
+           
+        transaksi = []
         print(listTemp)
+        print("Data Tersimpan")
